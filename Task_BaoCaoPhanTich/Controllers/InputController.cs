e891dfd8774907c5bfadb8e64b49449f4406140b
huaduybang <banghuaduy@gmail.com>
2020-07-24 15:46:27 +0700

    create-project

---
+++ Task_BaoCaoPhanTich/Controllers/InputController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task_BaoCaoPhanTich.Controllers
+{
+    public class InputController : Controller
+    {
+        // GET: Input
+        public ActionResult Input()
+        {
+            return View();
+        }
+    }
+}
